af57e5cc2718938ce31d9078c3a63acbedc68d74
KLCaron <kellencaron@gmail.com>
2024-02-11 19:50:11 -0700

    just getting this code in the repo

---
+++ GameServer/GameState.java
@@ -0,0 +1,78 @@
+package GameServer;
+
+import java.io.*;
+
+class GameState {
+    private String state;
+    private int attempts;
+    private String[] words;
+    private Puzzle puzzle;
+
+    public GameState() {
+        this.state = Constants.IDLE_STATE;
+    }
+
+    public GameState(int attempts, String[] words) {
+        this.state = Constants.PLAY_STATE;
+        this.attempts = attempts;
+        this.words = words;
+        this.puzzle = new Puzzle(this.words);
+    }
+
+    public GameState(BufferedReader reader) throws IOException {
+        this.state = ParseInput.parseStringValue(reader);
+        if (this.state.equals(Constants.PLAY_STATE)) {
+            this.attempts = ParseInput.parseIntValue(reader);
+            String wordsData = ParseInput.parseStringValue(reader);
+            this.words = wordsData.split(",");
+            this.puzzle = new Puzzle(this.words[0], reader);
+        }
+    }
+
+    public String getState() {
+        return this.state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public int getAttempts() {
+        return this.attempts;
+    }
+
+    public void decrementAttempts() {
+        this.attempts--;
+    }
+
+    public String[] getWords() {
+        return this.words;
+    }
+
+    public Puzzle getPuzzle() {
+        return this.puzzle;
+    }
+
+    public String getGameStateString() {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        stringBuilder.append("State;").append(this.state).append("\n");
+
+        if (this.state.equals(Constants.PLAY_STATE)) {
+            stringBuilder.append("Attempts;").append(this.attempts).append("\n");
+
+            stringBuilder.append("Words;");
+            for (int i = 0; i < this.words.length; i++) {
+                stringBuilder.append(this.words[i]);
+                if (i < words.length - 1) {
+                    stringBuilder.append(", ");
+                }
+            }
+
+            stringBuilder.append("\n").append(this.puzzle.getPuzzleString());
+            stringBuilder.append("$\n").append(this.puzzle.getSolutionString());
+        }
+        String gameStateString = stringBuilder.toString();
+        return gameStateString;
+    }
+}
---
+++ GameServer/UserData.java
@@ -0,0 +1,47 @@
+package GameServer;
+
+import java.io.*;
+
+class UserData {
+    private String username;
+    private int score;
+    private GameState gameState;
+
+    public UserData(String username) {
+        this.username = username;
+        this.score = 0;
+        this.gameState = new GameState();
+    }
+
+    public UserData(String username, int score, BufferedReader reader) throws IOException {
+        this.username = username;
+        this.score = score;
+        this.gameState = new GameState(reader);
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public int getScore() {
+        return this.score;
+    }
+
+    public GameState getGameState() {
+        return this.gameState;
+    }
+
+    public void setGameState(GameState gameState) {
+        this.gameState = gameState;
+    }
+
+    public String getUserDataString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("Username;").append(this.username).append("\n");
+        stringBuilder.append("Score;").append(this.score).append("\n");
+        stringBuilder.append(this.gameState.getGameStateString());
+
+        String userDataString = stringBuilder.toString();
+        return userDataString;
+    }
+}
